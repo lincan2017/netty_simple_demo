8bb3972d73bae06f18e4d50b9e520a392b71a708
lincan <gd_lincan@126.com>
2019-02-21 17:24:13 +0800

    accomplish NIOServer by Netty

---
+++ src/main/java/netty/Client.java
@@ -0,0 +1,38 @@
+package netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.util.Date;
+
+/**
+ * @author : Lin Can
+ * @date : 2019/2/21 17:18
+ */
+public class Client {
+    public static void main(String[] args) throws InterruptedException {
+        Bootstrap bootstrap = new Bootstrap();
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                });
+
+        Channel channel = bootstrap.connect("localhost", 8000).channel();
+
+        while (true) {
+            channel.writeAndFlush(new Date() + " hello NettyServer!!!");
+            Thread. sleep(5000);
+        }
+
+    }
+}
---
+++ src/main/java/netty/Server.java
@@ -0,0 +1,39 @@
+package netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * @author : Lin Can
+ * @date : 2019/2/21 17:11
+ */
+public class Server {
+    public static void main(String[] args) {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+
+        serverBootstrap
+                .group(boss, worker)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) {
+                        nioSocketChannel.pipeline().addLast(new StringDecoder());
+                        nioSocketChannel.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext channelHandlerContext, String msg) {
+                                System.out.println(msg);
+                            }
+                        });
+                    }
+                }).bind(8000);
+    }
+}
