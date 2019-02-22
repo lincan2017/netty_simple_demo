a838c08e1e07604d808069e89938f231ca02a37e
lincan <gd_lincan@126.com>
2019-02-22 14:39:45 +0800

    a netty client test whether the simple netty server start successfully

---
+++ src/main/java/netty/Client.java
@@ -0,0 +1,40 @@
+package netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.util.Date;
+
+/**
+ * 用于测试 simple netty server
+ * @author : Lin Can
+ * @date : 2019/2/22 11:06
+ */
+public class Client {
+    public static void main(String[] args) throws InterruptedException {
+        Bootstrap bootstrap = new Bootstrap();
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                });
+
+        Channel channel = bootstrap.connect("localhost", 136).channel();
+
+        while (true) {
+            channel.writeAndFlush(new Date() + " Wow!!! NettyServer !!!");
+            Thread. sleep(5000);
+        }
+
+    }
+}
+
