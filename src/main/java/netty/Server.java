499e09a739e85957ff968c90a8ad0f4089bf9af6
lincan <gd_lincan@126.com>
2019-02-22 11:25:34 +0800

    Auto increment port when the server binding fails

--- src/main/java/netty/Server.java
+++ src/main/java/netty/Server.java
@@ -1,10 +1,14 @@
 package netty;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
 
 /**
  * @author : Lin Can
@@ -24,10 +28,34 @@ public class Server {
         serverBootstrap.childHandler(new ChannelInitializer<NioSocketChannel>() {
             @Override
             protected void initChannel(NioSocketChannel ch) throws Exception {
+                ch.pipeline().addLast(new StringDecoder());
+                ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                    @Override
+                    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String msg) {
+                        System.out.println(msg);
+                    }
+                });
             }
         });
 
         //绑定端口
-        serverBootstrap.bind(8000);
+        bind(serverBootstrap,135);
+    }
+
+    /**
+     * 递归绑定端口，端口递增
+     * @param bootstrap 服务器启动引导类
+     * @param port 初始端口
+     */
+    private static void bind(ServerBootstrap bootstrap, int port) {
+        ChannelFuture channelFuture = bootstrap.bind(port);
+        channelFuture.addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println(port + " port bind success!!");
+                return;
+            }
+            // 如果绑定失败，则端口递增
+            bind(bootstrap, port + 1);
+        });
     }
 }
